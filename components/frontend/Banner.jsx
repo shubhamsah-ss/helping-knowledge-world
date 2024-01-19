5f461d7b2a3dfca4c9056f3adbe14813ac5627be
Shubham Sah <shubham.6552@gmail.com>
2024-01-19 13:15:45 +0530

    fetching current banner

--- components/frontend/Banner.jsx
+++ components/frontend/Banner.jsx
@@ -1,6 +1,25 @@
+"use client"
+import { makeGetRequest } from "@/lib/apiRequest";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
+  
+  const [banner, setBanner] = useState(null)
+
+  async function getBanner() {
+    try {
+      const bannerData = await makeGetRequest("banners/current")
+      console.log(bannerData)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getBanner();
+  }, [])
+
   return (
     <div>
         <Image
