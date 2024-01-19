d28dd81921941618cf7be4c9755561232317f933
Shubham Sah <shubham.6552@gmail.com>
2024-01-19 18:45:45 +0530

    banner image 4

--- components/frontend/Banner.jsx
+++ components/frontend/Banner.jsx
@@ -1,40 +1,43 @@
-"use client"
+"use client";
 import { makeGetRequest } from "@/lib/apiRequest";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Banner({ status, setStatus }) {
-  
-  const [banner, setBanner] = useState(null)
-  if (status) console.log(status)
+  const [banner, setBanner] = useState(null);
+
+  if (status) console.log(status);
 
   async function getBanner() {
     try {
-      const bannerData = await makeGetRequest("banners/current")
-      console.log(bannerData)
-      setBanner(bannerData)
-      if(setStatus) setStatus(false)
+      const timestamp = new Date().getTime(); // Add a timestamp as a cache buster
+      const bannerData = await makeGetRequest(
+        `banners/current?timestamp=${timestamp}`
+      );
+      console.log("Banner Data:", bannerData);
+      setBanner(bannerData);
+      setStatus(false);
     } catch (error) {
-      console.log(error)
+      console.error("Error fetching banner data:", error);
     }
   }
 
   useEffect(() => {
-    getBanner()
-  }, [status])
+    getBanner();
+  }, [status]);
 
   return (
     <div>
-  {
-    banner && <Image
-    src={banner.image[0]}
-    alt="banner image"
-    width={1000}
-    height={1000}
-    priority={true}
-    className="w-full h-full md:h-[300px] lg:h-[450px] mx-auto object-contain md:object-fill"
-  />
-  }        
+      {banner && (
+        <Image
+          src={banner.image[0]}
+          alt="banner image"
+          width={1000}
+          height={1000}
+          loading="lazy"
+          className="w-full h-full md:h-[300px] lg:h-[450px] mx-auto object-contain md:object-fill"
+        />
+      )}
     </div>
   );
 }
